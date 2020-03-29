9c8dcdb428b788bfda3450cf69a07da98775c97d
Luis Geniole <luis.geniole@gmail.com>
2020-03-29 01:04:25 -0300

    Rename tests and ignore transpiled .js files.

--- src/controllers/StudentController.ts
+++ src/controllers/StudentController.ts
@@ -3,16 +3,8 @@ import StudentORM from '../database/StudentORM';
 import StudentViews from '../views/StudentViews';
 import '../interfaces/StringInterface';
 
-// TODO use yup for the validations
 class StudentController {
-  // AKA adicionarAluno
   addStudent(name: string): string {
-    /**
-     * Essa função irá receber uma *string* que é nome do aluno a ser criado.
-     * E seguindo o modelo de aluno, o mesmo deverá ser inserido na lista de
-     * alunos. A função deve devolver um feedback de sucesso, caso o aluno seja
-     * inserido corretamente.
-     */
     try {
       if (name.isEmpty()) throw 'Nome do aluno nao pode ser vazio.';
 
@@ -31,25 +23,11 @@ class StudentController {
     return `Aluno ${name} adicionado com sucesso`;
   }
 
-  // AKA listarAlunos
   listStudents(): string {
-    /**
-     * Com essa função o usuário poderá ver todos os alunos cadastrados
-     * atualmente no sistema. Vale dizer que As informações deverão ser
-     * exibidas em um formato amigável.
-     */
-
     return StudentViews.showStudents(StudentORM.all());
   }
 
-  // AKA buscarAluno
   findStudent(name: string): Student {
-    /**
-     * Por meio dessa função, podemos pesquisar um aluno por nome na lista de
-     * aluno. Ela deverá exibir um feedback, tanto para quando encontrar o
-     * aluno, tanto quando não encontrar. E deverá devolver um aluno em seu
-     * retorno.
-     */
     try {
       if (name.isEmpty()) throw 'Nome do aluno nao pode ser vazio.';
 
@@ -63,14 +41,7 @@ class StudentController {
     }
   }
 
-  // AKA matricularAluno
   enrollStudent(targetStudent: Student, courseName: string): Student {
-    /**
-     * Essa funcionalidade irá permitir, cadastrar um aluno em um curso.
-     * Essa função só poderá ser executada em um aluno já devidamente cadastrado
-     * no sistema, e deverá armazenar a data atual no momento da matricula.
-     * Lembre-se de exibir o feedback para o usuário.
-     */
     try {
       if (courseName.isEmpty()) throw 'Nome do aluno nao pode ser vazio.';
 
@@ -86,14 +57,7 @@ class StudentController {
     }
   }
 
-  // AKA aplicarFalta
   addAbsence(targetStudent: Student): Student {
-    /**
-     * Ao receber um aluno devidamente cadastrado em nossa lista. Você deverá
-     * incrementar uma falta ao aluno. Você deverá dar um feedback ao concluir
-     * a tarefa. Só poderá aplicar falta em aluno se o mesmo tiver matriculado
-     * em um curso.
-     */
     try {
       targetStudent = StudentORM.find(targetStudent);
 
@@ -109,14 +73,7 @@ class StudentController {
     }
   }
 
-  // AKA aplicarNota
   addGrade(targetStudent: Student, grade: number): Student {
-    /**
-     * Ao receber um aluno devidamente cadastrado em nossa lista. Você deverá
-     * adicionar uma nota ao aluno na sua lista de notas. Você deverá dar um
-     * feedback ao concluir a tarefa. Só poderá aplicar nota em aluno se o
-     * mesmo tiver matriculado em um curso.
-     */
     try {
       if (grade < 0 || grade > 10) throw 'Nota precisa ser entre 0 e 10.';
 
@@ -134,14 +91,7 @@ class StudentController {
     }
   }
 
-  // AKA aprovarAluno
   flunkOrPass(targetStudent: Student): string {
-    /**
-     * Ao receber um aluno devidamente cadastrado em nossa lista, deverá dizer
-     * se o mesmo está aprovado ou não. Os critérios de aprovação são: ter no
-     * máximo 3 faltas e média 7 em notas. Só o aluno só poderá ser aprovado se
-     * o mesmo tiver matriculado em um curso.
-     */
     try {
       targetStudent = StudentORM.find(targetStudent);
 
